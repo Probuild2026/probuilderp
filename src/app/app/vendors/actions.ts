46b58285d624240c26b148f6673dd18f1bbe0737
Probuild2026 <roshannatasha2188@gmail.com>
2026-03-01 11:44:01 +0530

    Fix vendor update auth session (getServerSession)

--- src/app/app/vendors/actions.ts
+++ src/app/app/vendors/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 import { Prisma } from "@prisma/client";
 
 import { vendorCreateSchema, vendorMergeSchema, vendorUpdateSchema } from "@/lib/validators/vendor";
